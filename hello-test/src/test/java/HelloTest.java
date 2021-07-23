7d119de523db3615f8267be4d761fc743c967195
up1 <somkiat.p@gmail.com>
2021-07-23 11:53:04 +0700

    Done with case 01

--- hello-test/src/test/java/HelloTest.java
+++ hello-test/src/test/java/HelloTest.java
@@ -1,5 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloTest {
-
+    // f(name) = "Hello somkiat"
+    @Test
+    public void case01() {
+        // Arrange
+        Hello hello = new Hello();
+        String name = "somkiat";
+        // Act
+        String actualResult = hello.sawadee(name);
+        // Assert
+        assertEquals("Hello somkiat", actualResult);
+    }
 }
