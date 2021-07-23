40c2b5dea195bf51ba7fe0d154d709867a9a3c6b
up1 <somkiat.p@gmail.com>
2021-07-23 17:20:51 +0700

    Move from switch-case to array

--- hello-test/src/main/java/TennisGame1.java
+++ hello-test/src/main/java/TennisGame1.java
@@ -12,10 +12,11 @@ public class TennisGame1 {
     }
 
     public void wonPoint(String playerName) {
-        if (Objects.equals(playerName, "player1"))
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (Objects.equals(playerName, this.player1Name)) {
+            m_score1++;
+        } else {
+            m_score2++;
+        }
     }
 
     public String getScore() {
@@ -39,10 +40,10 @@ public class TennisGame1 {
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            if (minusResult == 1) score = new StringBuilder("Advantage ").append(this.player1Name);
+            else if (minusResult == -1) score = new StringBuilder("Advantage ").append(this.player2Name);
+            else if (minusResult >= 2) score = new StringBuilder("Win for ").append(this.player1Name);
+            else score = new StringBuilder("Win for ").append(this.player2Name);
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) {
@@ -51,20 +52,8 @@ public class TennisGame1 {
                     score.append("-");
                     tempScore = m_score2;
                 }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
+                String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty" };
+                score.append(scoreResults[tempScore]);
             }
         }
         return score.toString();
