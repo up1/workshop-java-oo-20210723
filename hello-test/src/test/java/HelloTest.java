d4b5a6b1954f6d3a7887a08f64c6ae374b122b33
up1 <somkiat.p@gmail.com>
2021-07-23 11:54:31 +0700

    Add test case name

--- hello-test/src/test/java/HelloTest.java
+++ hello-test/src/test/java/HelloTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HelloTest {
     // f(name) = "Hello somkiat"
     @Test
+    @DisplayName("ทำการทดสอบ .....")
     public void case01() {
         // Arrange
         Hello hello = new Hello();
