b789005c0e5af589da21027e6399f924cd221a47
up1 <somkiat.p@gmail.com>
2021-07-23 14:18:19 +0700

    Success with test case 02

--- hello-test/src/test/java/MyRangeTest.java
+++ hello-test/src/test/java/MyRangeTest.java
@@ -1,9 +1,18 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyRangeTest {
+
+    @Test
+    @DisplayName("ข้อมูล (1,5] นั้นจะขึ้นต้นด้วย ( (exclude) ผลที่ได้คือ false")
+    public void startWithExclude() {
+        MyRange myRange = new MyRange("(1,5]");
+        boolean result = myRange.isStartWithInclude();
+        assertFalse(result);
+    }
 
     @Test
     @DisplayName("ข้อมูล [1,5] นั้นจะขึ้นต้นด้วย [ (include) ผลที่ได้คือ true")
