59a082a66c6c78a828dcc2fd7589348143fc8e17
up1 <somkiat.p@gmail.com>
2021-07-24 12:23:09 +0700

    Working with DIP and DI

--- solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
+++ solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
@@ -3,7 +3,17 @@ package dip;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+// Mock
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
 
 class GenerateIdServiceTest {
 
@@ -11,6 +21,7 @@ class GenerateIdServiceTest {
     @DisplayName("ต้องได้ id=XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        service.setRandom(new Random7());
         String id = service.getId();
         assertEquals("XYZ7", id);
     }
