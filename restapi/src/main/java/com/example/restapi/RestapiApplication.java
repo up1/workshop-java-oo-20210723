a9b8be4940aedd7ee91554a6b9e49805ef4465cb
up1 <somkiat.p@gmail.com>
2021-07-24 15:44:30 +0700

    Add bean random type

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -3,9 +3,17 @@ package com.example.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class RestapiApplication {
+
+	@Bean
+	public Random createRandom() {
+		return new Random();
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx
