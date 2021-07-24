4bd5b26ab97e671db3130235e52213aa6e2f4470
up1 <somkiat.p@gmail.com>
2021-07-24 09:31:26 +0700

    Finally with do one thing

--- hello-test/src/main/java/TennisGame1.java
+++ hello-test/src/main/java/TennisGame1.java
@@ -21,9 +21,23 @@ public class TennisGame1 {
 
     public String getScore() {
         String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-        // All
-        if (isAll()) {
-            return scoreResults[m_score1] + "-" + "All";
+
+        int diffScore = m_score1 - m_score2;
+
+        // Win
+        if ((m_score1 >= 4 || m_score2 >= 4) && Math.abs(diffScore) >= 2) {
+            StringBuilder score = new StringBuilder();
+            if (diffScore >= 2) score = new StringBuilder("Win for ").append(this.player1Name);
+            else score = new StringBuilder("Win for ").append(this.player2Name);
+            return score.toString();
+        }
+
+        // Advantage
+        if ((m_score1 >= 4 || m_score2 >= 4) && Math.abs(diffScore) == 1) {
+            StringBuilder score = new StringBuilder();
+            if (diffScore == 1) score = new StringBuilder("Advantage ").append(this.player1Name);
+            else if (diffScore == -1) score = new StringBuilder("Advantage ").append(this.player2Name);
+            return score.toString();
         }
 
         // Deuce
@@ -32,15 +46,9 @@ public class TennisGame1 {
             return "Deuce";
         }
 
-        // ???? Advantage and Win ????
-        if (m_score1 >= 4 || m_score2 >= 4) {
-            StringBuilder score = new StringBuilder();
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage ").append(this.player1Name);
-            else if (minusResult == -1) score = new StringBuilder("Advantage ").append(this.player2Name);
-            else if (minusResult >= 2) score = new StringBuilder("Win for ").append(this.player1Name);
-            else score = new StringBuilder("Win for ").append(this.player2Name);
-            return score.toString();
+        // All
+        if (isAll()) {
+            return scoreResults[m_score1] + "-" + "All";
         }
 
         // Normal game
