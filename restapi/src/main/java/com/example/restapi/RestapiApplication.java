5ba1e5aab5891047b811a7e9a540ac2dfc8b9c07
up1 <somkiat.p@gmail.com>
2021-07-24 16:32:54 +0700

    Create rest template bean

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,13 +2,20 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
 
 @SpringBootApplication
 public class RestapiApplication {
+
+	@Bean
+	public RestTemplate createRestTemplate() {
+		return new RestTemplateBuilder().build();
+	}
 
 	@Bean
 	public Random createRandom() {
