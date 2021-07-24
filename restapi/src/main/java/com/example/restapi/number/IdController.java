593132e9f6258a758f9c0b2b99f2b1b4db10cbbd
up1 <somkiat.p@gmail.com>
2021-07-24 16:26:24 +0700

    Working with external api

--- restapi/src/main/java/com/example/restapi/number/IdController.java
+++ restapi/src/main/java/com/example/restapi/number/IdController.java
@@ -1,6 +1,8 @@
 package com.example.restapi.number;
 
 import com.example.restapi.GenerateIdService;
+import com.example.restapi.user.UserGateway;
+import com.example.restapi.user.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,19 @@ public class IdController {
     @Autowired
     private GenerateIdService service;
 
+    @Autowired
+    private UserGateway userGateway;
+
     @GetMapping("/id")
     public DataResponse generateId() {
         String result = service.getId();
         return new DataResponse(result);
+    }
+
+    @GetMapping("/user")
+    public UserResponse getUser() {
+        UserResponse user = userGateway.getUserById(1);
+        return user;
     }
 
 }
