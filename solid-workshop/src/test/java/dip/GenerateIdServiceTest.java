d313305509467b5f4f4d5aa3c6bebc9e1a6eb74a
up1 <somkiat.p@gmail.com>
2021-07-24 12:30:34 +0700

    Demo with Mock

--- solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
+++ solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
@@ -8,10 +8,15 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 // Mock
-class Random7 extends Random {
+class RandomX extends Random {
+    long number;
+    public RandomX(long seed) {
+        number = seed;
+    }
+
     @Override
     public int nextInt(int bound) {
-        return 7;
+        return (int) number;
     }
 }
 
@@ -21,7 +26,7 @@ class GenerateIdServiceTest {
     @DisplayName("ต้องได้ id=XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
-        service.setRandom(new Random7());
+        service.setRandom(new RandomX(7)); // Test Double = Stub
         String id = service.getId();
         assertEquals("XYZ7", id);
     }
